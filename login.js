230aac6548c0cc5a1114fb7f6945b2121ed765ee
WeiXiangShen <e6031583@gmail.com>
2022-01-13 22:28:15 +0800

    initial commit

---
+++ login.js
@@ -0,0 +1,20 @@
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener("click", processFormData);
+
+function processFormData() {
+        const nameElement = document.getElementById("username");
+        const name = nameElement.value;
+        const passElement = document.getElementById("pass");
+        const pass = passElement.value;
+        if(name == "admin" & pass == 123456){
+                document.form1.action = "adminIndex.html"
+        }
+        else if(name == "user" & pass == 654321){
+                document.form1.action = "user.html"
+        }
+        else{
+                alert("wrong");
+                document.form1.action = "login.html"
+        }
+        // alert("yor account is  " + name + "\npassword is " + email);
+}
